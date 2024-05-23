426da4b41f40697b96253f0f4a70fac0d3bf1a98
izzyara <izzybzb@gmail.com>
2024-05-23 14:59:05 -0400

    a

--- ApplicationRobot.java
+++ ApplicationRobot.java
@@ -5,11 +5,35 @@ public class ApplicationRobot{
     public static void main(String[]args) throws Exception{
     
         Robot emma = new Robot();
+        emma.setAutoDelay(200);
 
-        emma.mouseMove(0,0);
-        Color color = emma.getPixelColor(1500,500);
+        emma.mouseMove(0,1080);
         emma.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        emma.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
-        System.out.println(color);
+        emma.mouseMove(100,820);
+        emma.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        emma.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        
+        emma.mouseMove(1310,500);
+        emma.setAutoDelay(1000);
+        emma.mouseMove(1320,500);
+        emma.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        emma.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        emma.mouseMove(1320,800);
+        emma.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        emma.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        emma.mouseMove(820,150);
+        emma.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        emma.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+        emma.mouseMove(820,265);
+        emma.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        emma.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+
+
     }
 }
